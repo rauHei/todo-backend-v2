f90cedafca746e6b35f18cb41cacc89e483fd1bf
rauHei <101550372+rauHei@users.noreply.github.com>
2022-08-23 14:40:52 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
-
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
+const http = require('http')
 const config = require('./utils/config')
-const noterouter = require('./routes/noterouter')
-const path = require('path')
 
-const middleware = require('./utils/middleware')
+const app = require('./app');
 
+const server = http.createServer(app)
 
-// mongo here...
-
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connection to MongoDB:', error.message)
-    })
-
-// this allows connection from different domains and ports
-app.use(cors())
-
-
-// convert json string to json object (from request)
-app.use(express.json())
-
-app.use(express.static('build'))
-
-
-
-app.use('/api/todos', noterouter)
-
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "./build/index.html")
-    );
+// app listen port 3001
+server.listen(config.PORT, () => {
+    console.log(`Todo app listening on port ${config.PORT}`)
 })
-
-app.use(middleware.requestLogger)
-
-module.exports = app;
-
-
-
-
-
-
